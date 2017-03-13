fbb8f62428ce36739bc0b3f28c27428966007ae4
wanglei <664125833@qq.com>
2017-03-13 15:26:22 +0800

    ss

---
+++ jsonData/listData.js
@@ -0,0 +1,184 @@
+listData=
+[
+	{
+		"name": "实例",
+		"open": true,
+		"children": [
+			{
+				"name": "ch1",
+				"children": [
+					{
+						"name": "test1",
+						"url": "html/ch1/test1.html"
+					}, {
+						"name": "test2",
+						"url": "html/ch1/test2.html"
+					}, {
+						"name": "test3",
+						"url": "html/ch1/test3.html"
+					}
+				]
+			}, {
+				"name": "ch2",
+				"children": [
+					{
+						"name": "test1",
+						"url": "html/ch2/test1.html"
+					}, {
+						"name": "test2",
+						"url": "html/ch2/test2.html"
+					}, {
+						"name": "test3",
+						"url": "html/ch2/test3.html"
+					}, {
+						"name": "test4",
+						"url": "html/ch2/test4.html"
+					}, {
+						"name": "test6",
+						"url": "html/ch2/test6.html"
+					}, {
+						"name": "test7",
+						"url": "html/ch2/test7.html"
+					}, {
+						"name": "test8",
+						"url": "html/ch2/test8.html"
+					}, {
+						"name": "test9",
+						"url": "html/ch2/test9.html"
+					}, {
+						"name": "test10",
+						"url": "html/ch2/test10.html"
+					}, {
+						"name": "test11",
+						"url": "html/ch2/test11.html"
+					}, {
+						"name": "test12",
+						"url": "html/ch2/test12.html"
+					}, {
+						"name": "test13",
+						"url": "html/ch2/test13.html"
+					}, {
+						"name": "test14",
+						"url": "html/ch2/test14.html"
+					}
+				]
+			}, {
+				"name": "ch3",
+				"children": [
+					{
+						"name": "test1",
+						"url": "html/ch3/test1.html"
+					}, {
+						"name": "test2",
+						"url": "html/ch3/test2.html"
+					}, {
+						"name": "test3",
+						"url": "html/ch3/test3.html"
+					}, {
+						"name": "test4",
+						"url": "html/ch3/test4.html"
+					}, {
+						"name": "test5",
+						"url": "html/ch3/test5.html"
+					}, {
+						"name": "test6",
+						"url": "html/ch3/test6.html"
+					}, {
+						"name": "test7",
+						"url": "html/ch3/test7.html"
+					}, {
+						"name": "test8",
+						"url": "html/ch3/test8.html"
+					}, {
+						"name": "test9",
+						"url": "html/ch3/test9.html"
+					}, {
+						"name": "test10",
+						"url": "html/ch3/test10.html"
+					}, {
+						"name": "test11",
+						"url": "html/ch3/test11.html"
+					}, {
+						"name": "test12",
+						"url": "html/ch3/test12.html"
+					}, {
+						"name": "test13",
+						"url": "html/ch3/test13.html"
+					}
+				]
+			},{
+				"name": "ch4",
+				"children": [
+					{
+						"name": "test1",
+						"url": "html/ch4/test1.html"
+					}, {
+						"name": "test3",
+						"url": "html/ch4/test3.html"
+					}, {
+						"name": "test4",
+						"url": "html/ch4/test4.html"
+					}, {
+						"name": "test5",
+						"url": "html/ch4/test5.html"
+					}, {
+						"name": "test6",
+						"url": "html/ch4/test6.html"
+					}, {
+						"name": "test8",
+						"url": "html/ch4/test8.html"
+					}, {
+						"name": "test9",
+						"url": "html/ch4/test9.html"
+					}, {
+						"name": "test10",
+						"url": "html/ch4/test10.html"
+					}, {
+						"name": "test11",
+						"url": "html/ch4/test11.html"
+					}
+				]
+			},{
+				"name": "没有子节点",
+				"isParent": true
+			}
+		]
+	}, {
+		"name": "教程",
+		"children": [
+			{
+				"name": "ch2",
+				"url": "html/tech/ch2.html"
+			},{
+				"name": "ch3",
+				"url": "html/tech/ch3.html"
+			},{
+				"name": "ch4",
+				"url": "html/tech/ch4.html"
+			}
+		]
+	}, {
+		"name": "点名",
+		"children": [
+			{
+				"name": "汉班点名",
+				"url": "dianming.html"
+			},
+			{
+				"name": "蒙班点名",
+				"url": "dianming2.html"
+			}
+		]
+	},{
+		"name": "相关学习网站",
+		"children": [
+			{
+				"name": "菜鸟教程",
+				"url": "http://www.runoob.com/"
+			},{
+				"name":	"w3school",
+				"url":	"http://www.w3school.com.cn/"
+			}
+		]
+	}
+]
